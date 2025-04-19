fb4fa3f87f412617b22af78d224c301049a639b9
chiarabuono <s7687956@studenti.unige.it>
2025-04-19 08:48:20 +0000

    Environment alert

--- src/assignments/src/environment_alert.py
+++ src/assignments/src/environment_alert.py
@@ -4,29 +4,41 @@ import random
 
 from std_msgs.msg import String
 from assignments.msg import Cracks, Crack
+from nav_msgs.msg import Odometry
 
+trascurable_severities = ["Negligible", "Marginal"]
 
 class EnvironmentAlert:
     def __init__(self):
         rospy.init_node('environment_alert')
         self.subscriber = rospy.Subscriber("wall_classification", String, self.send_alarm)
+        self.odom_sub = rospy.Subscriber("/mobile_base_controller/odom", Odometry, self.odom_callback)
+        self.publisher = rospy.Publisher("/environment_alarm", Crack, queue_size= 1)
+
+        self.robot_position = Odometry()
 
         rospy.loginfo(f"Environment alert node active")
+
+    def odom_callback(self, msg):
+        self.robot_position = msg
+
+    def compute_danger_position(self):
+        rospy.loginfo("Computing danger position")
         
+        danger = Crack()
+        danger.x = self.robot_position.pose.pose.position.x + random.randint(-100, 100)
+        danger.y = self.robot_position.pose.pose.position.y + random.randint(-100, 100)
+        danger.severity = self.severity
+
+        return danger
 
     def send_alarm(self, msg):
-        num = random.randint(0, 10)
+        if msg.data and (msg.data not in trascurable_severities):
+            self.severity = msg.data
+            self.publisher.publish(self.compute_danger_position()) 
+            rospy.loginfo(f"Sending alarm with wall position")   
 
-        if (msg.n_cracks == 0): 
-            rospy.loginfo(f"No crack or week walls")
-
-        else:
-            class_classification = self.wall_classification(msg)
-            self.publisher.publish(class_classification)
-            rospy.loginfo(f"Wall classification: {class_classification}")
     
-
-
 
     def run(self):
         rospy.spin()
