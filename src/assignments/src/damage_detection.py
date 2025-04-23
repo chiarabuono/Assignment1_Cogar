b9a0c48564c90552e3f3c131a58e47c390b5fc29
chiarabuono <s7687956@studenti.unige.it>
2025-04-23 18:16:06 +0000

    wall detection working

--- src/assignments/src/damage_detection.py
+++ src/assignments/src/damage_detection.py
@@ -11,24 +11,25 @@ class DamageDetection:
     def __init__(self):
         rospy.init_node('damage_detection')
         self.service = rospy.Service('/wall_classification', WallStatus, self.wall_classification)
+        self.publisher = rospy.Publisher()
 
         rospy.loginfo(f"Damage detection node active")
         
     
     def wall_classification(self, msg):
         severity_counts = {}
-        for i in range(msg.n_cracks):
-            if (msg.cracks[i].severity.data not in severity_counts): 
-                severity_counts[msg.cracks[i].severity.data] = 1
-            else: 
-                severity_counts[msg.cracks[i].severity.data] += 1
+        for i in range(msg.cracks.n_cracks):
+            severity = msg.cracks.cracks[i].severity.data
+            severity_counts[severity] = severity_counts.get(severity, 0) + 1
+
 
         max_severity = max(severity_counts, key=severity_counts.get)
+        severity_msg = String(data=max_severity)
 
         #TODO: compute the wall position
         x_wall = random.randint(0, 10)
         y_wall = random.randint(0, 10)
-        wall = Crack(x_wall, y_wall, max_severity)
+        wall = Crack(x_wall, y_wall, severity_msg)
 
         return WallStatusResponse(wall)
 
